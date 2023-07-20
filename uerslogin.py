d5a4f24927ca64e290c7cd1792de4a6a04b64952
Ahoora Dolatabudi <126002362+Programmer803@users.noreply.github.com>
2023-07-20 19:15:33 +0330

    Update uerslogin.py
    Programmer803 req

--- uerslogin.py
+++ uerslogin.py
@@ -2,28 +2,34 @@
 user = {
     'name' : 'unknown',
     'password' : 'unknown',
+    'totalwins' : 0,
+    'totalgames' : 0
 }
+
+# user = dict.fromkeys(['name', 'passwords'], 'unknown')
 
 register = False
 
 while register == False :
+
     print('register. ')
+    
     name = input('please enter your name: ')
     password = input('please enter your pasword: ')
+    
     User = {
     'name' : name,
     'password' : password
     }
     user.update(User)
     register = True
+    print(User)
     if register == True :
         print('register successful. ')
         break
 
-print(user)
-print(User)
-
 while register == True :
+
     print('log in. ')
     name = input('please enter your name: ')
     password = input('please enter your pasword: ')
@@ -31,18 +37,18 @@ while register == True :
     'name' : name,
     'password' : password
     }
-    while not user.values() == User.values() :
+    
+    while not user["name"] == User["name"] and user["password"] == User["password"] :
+    
         print('invalid password/name. ')
         name = input('please enter your name: ')
         password = input('please enter your pasword: ')
+        
         User = {
         'name' : name,
         'password' : password
         }
-        if user.values() == User.values() :
-            print('log in successful. wellcome,' + user['name'] + '.')
-            
-
-
-            
-
+        
+    if user["name"] == User["name"] and user["password"] == User["password"] :
+     
+        print('log in successful. wellcome,' + user['name'] + '.')
